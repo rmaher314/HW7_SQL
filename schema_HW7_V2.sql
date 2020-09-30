ee764103bb3ef9f7bce643666a908d45b527af79
Regan <regan_maher@outlook.com>
2020-09-30 18:38:21 -0500

    Got all files to import, created a new schema and organized the folder.

---
+++ schema_HW7_V2.sql
@@ -0,0 +1,93 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Titles" (
+    "title_id" VARCHAR(5)  NOT NULL ,
+    "title" VARCHAR(20)  NOT NULL ,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+    )
+)
+
+GO
+
+CREATE TABLE "Employees" (
+    "emp_no" INT  NOT NULL ,
+    "emp_title_id" VARCHAR(5)  NOT NULL ,
+    "birth_date" DATE  NOT NULL ,
+    "first_name" VARCHAR(40)  NOT NULL ,
+    "last_name" VARCHAR(40)  NOT NULL ,
+    "sex" CHAR(1)  NOT NULL ,
+    "hire_date" DATE  NOT NULL ,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+    )
+)
+
+GO
+
+CREATE TABLE "Saleries" (
+    "emp_no" INT  NOT NULL ,
+    "salary" INT  NOT NULL ,
+    CONSTRAINT "pk_Saleries" PRIMARY KEY (
+        "emp_no"
+    )
+)
+
+GO
+
+CREATE TABLE "Department_Managers" (
+    "dept_no" VARCHAR(5)  NOT NULL ,
+    "emp_no" INT  NOT NULL ,
+    CONSTRAINT "pk_Department_Managers" PRIMARY KEY (
+        "emp_no"
+    )
+)
+
+GO
+
+CREATE TABLE "Department_Emp" (
+    "emp_no" INT  NOT NULL ,
+    "dept_no" VARCHAR(5)  NOT NULL ,
+    CONSTRAINT "pk_Department_Emp" PRIMARY KEY (
+        "emp_no", "dept_no"
+    )
+)
+
+GO
+
+CREATE TABLE "Departments" (
+    "dept_no" VARCHAR(5)  NOT NULL ,
+    "dept_name" VARCHAR(25)  NOT NULL ,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+    )
+)
+
+GO
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id")
+GO
+
+ALTER TABLE "Saleries" ADD CONSTRAINT "fk_Saleries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no")
+GO
+
+ALTER TABLE "Department_Managers" ADD CONSTRAINT "fk_Department_Managers_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no")
+GO
+
+ALTER TABLE "Department_Managers" ADD CONSTRAINT "fk_Department_Managers_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no")
+GO
+
+ALTER TABLE "Department_Emp" ADD CONSTRAINT "fk_Department_Emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no")
+GO
+
+ALTER TABLE "Department_Emp" ADD CONSTRAINT "fk_Department_Emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no")
+GO
+
