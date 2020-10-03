bde09fd1251b5b9dd53a132d70210718691bffe9
Regan <regan_maher@outlook.com>
2020-10-03 09:40:34 -0500

    Changed Table names to only be lower cased. Got questions 1,2,5 to output data.

--- queries_HW7.sql
+++ queries_HW7.sql
@@ -1,29 +1,38 @@
 --Listing the following details on each employee; employee number, last name, first name, sex and salary.
-SELECT * FROM "Saleries";
-SELECT * FROM "Employees";
+alter table "Departments" rename to departments
 
-SELECT Employees.emp_no,Employees.first_name, Employees.last_name, Employees.sex, Saleries.salary
-FROM "Saleries" as s1
-INNER JOIN "Employees" ON
-Employees.emp_no=s1.emp_no;
+SELECT * FROM salaries;
+SELECT * FROM employees;
+
+SELECT employees.emp_no, employees.first_name, employees.last_name, employees.sex, salaries.salary
+FROM salaries
+INNER JOIN employees ON
+employees.emp_no=salaries.emp_no;
+
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
 SELECT "first_name","last_name","hire_date"
-FROM "Employees"
+FROM employees
 WHERE (hire_date) >= '1986-01-01'
 and (hire_date) <= '1986-12-31';
 
 --List the manager of each department with the following information:  department number, deparment name, manager's employee nubmer, last name, first name.
-SELECT * FROM "Department_Managers";
-SELECT * FROM "Departments";
+SELECT * FROM department_managers;
+SELECT * FROM departments;
 
-SELECT Departments.dept_no,Departments.dept_name,Department_Managers.emp_no, Employees.last_name, Employees.sex
-FROM "Departments" 
-INNER JOIN "Department_Managers" ON
-Departments.dept_no=Department_Managers.dept_no
+SELECT departments.dept_no,departments.dept_name,department_managers.emp_no --employees.last_name, employees.sex
+FROM departments
+INNER JOIN department_managers ON
+departments.dept_no=department_managers.dept_no
 
 FROM "Departments_Managers";
 INNER JOIN "Employees" ON
 Department_Managers.emp_no=Employees.emp_no
 
 
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last name begins with "B"
+SELECT "first_name","last_name","sex"
+FROM employees
+WHERE 
+	first_name = 'Hercules'
+	and last_name like 'B%'
